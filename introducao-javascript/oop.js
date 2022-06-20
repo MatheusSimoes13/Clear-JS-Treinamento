c9f140c7a73ad11b3cc078f81fd7855389f12924
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-06-20 10:07:14 -0300

    Introdução ao javascript - Básico II

---
+++ introducao-javascript/oop.js
@@ -0,0 +1,20 @@
+class celular {
+
+    constructor(){
+
+        this.cor = "prata";
+
+    }
+
+    ligar(){
+
+        console.log("ligação");
+        return "ligando";
+
+    }
+
+}
+
+let objeto = new celular();
+
+console.log(objeto);
