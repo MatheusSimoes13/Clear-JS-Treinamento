156c75d57cb2336a22b42ffc8edf89910238b30f
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-06-21 11:01:09 -0300

    Botão ponto e correções de bugs com o ponto

--- calculadora/scripts/controller/CalcController.js
+++ calculadora/scripts/controller/CalcController.js
@@ -38,6 +38,8 @@ class CalcController{
 
     clearAll(){
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
         this.setLastNumberToDisplay();
     }
 
@@ -73,9 +75,6 @@ class CalcController{
     }
 
     getResult(){
-
-
-
         return eval(this._operation.join(""));
     }
 
@@ -100,7 +99,7 @@ class CalcController{
             this._lastNumber = this.getLastItem(false);
         }
         
-        let result = this.getResult;
+        let result = this.getResult();
 
         if(last == '%'){
 
@@ -161,9 +160,8 @@ class CalcController{
 
             if(this.isOperator(value)){
                 this.setLastOperation(value);
-            } else if(isNaN(value)){ 
-                
-            } else{
+            } 
+            else{
                 this.pushOperation(value);
 
                 this.setLastNumberToDisplay();
@@ -177,7 +175,7 @@ class CalcController{
             } else{
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
                 
@@ -189,6 +187,20 @@ class CalcController{
 
     setError(){
         this.displayCalc = "Error";
+    }
+
+    addDot(){
+        let lastOperation = this.getLastOperation();
+
+        if(typeof lastOperation ==='string' && lastOperation.split('').indexOf('.') > -1) return;
+
+        if(this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
     }
 
     execBtn(value){
@@ -221,7 +233,7 @@ class CalcController{
                 break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot();
                 break;
             case '0':
             case '1': 
