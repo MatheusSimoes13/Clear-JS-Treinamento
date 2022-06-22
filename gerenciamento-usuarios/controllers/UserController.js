2bf96e37d1f75b78483ad782d79b95b42ea57df8
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-06-22 10:57:05 -0300

    Operador Spread e Trabalhando com File Reader

--- gerenciamento-usuarios/controllers/UserController.js
+++ gerenciamento-usuarios/controllers/UserController.js
@@ -14,17 +14,43 @@ class UserController {
 
             event.preventDefault();
 
-            this.addLine(this.getValues());
+            let values = this.getValues();
+
+            this.getPhoto((content) => {
+                values.photo = content;
+
+                this.addLine(values);
+            });
 
         });
         
+    }
+
+    getPhoto(callback){
+
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item=>{
+            if (item.name === 'photo') {
+                return item;
+            }
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+            
+            callback(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
     }
 
     getValues(){
 
         let user = {};
 
-        this.formEl.elements.forEach(function (field, index){
+        [...this.formEl.elements].forEach(function (field, index){
 
             if(field.name == "gender"){
                 if(field.checked){
@@ -51,9 +77,9 @@ class UserController {
 
     addLine(dataUser){
 
-        this.tableId.innerHTML = `
+        this.tableEl.innerHTML = `
         <tr>
-            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin}</td>
