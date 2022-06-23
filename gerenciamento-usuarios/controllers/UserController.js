2cbce27be06f65023161ee06a3bbb1b26c4d21d4
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-06-23 09:46:00 -0300

    Trabalhando com edição de dados- editando a view, programando o evento no botão editar

--- gerenciamento-usuarios/controllers/UserController.js
+++ gerenciamento-usuarios/controllers/UserController.js
@@ -6,6 +6,13 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
+    }
+
+    onEdit(){
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click",e=>{
+            this.showPanelCreate();
+        });
     }
 
     onSubmit(){
@@ -19,7 +26,10 @@ class UserController {
 
             let values = this.getValues();
 
-            if (!values) return false;
+            if (!values){
+                btn.disabled = false;
+                return false;
+            } 
 
             this.getPhoto().then(
             (content)=>{
@@ -131,15 +141,30 @@ class UserController {
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
             `;
 
+        tr.querySelector(".btn-edit").addEventListener("click",e=>{
+            console.log(tr);
+            this.showPanelUpdate();
+        });
+
         this.tableEl.appendChild(tr);   
         
         this.updateCount();
+    }
+
+    showPanelCreate(){
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    }
+
+    showPanelUpdate(){
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     }
 
     updateCount(){
