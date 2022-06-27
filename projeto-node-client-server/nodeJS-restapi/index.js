8252914ec772c381b3cb229d96f9bded3f2bd18a
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-06-27 10:50:11 -0300

    Iniciando estrutura do projeto gerenciamento de usuarios restful api

---
+++ projeto-node-client-server/nodeJS-restapi/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+// let routesIndex = require('./routes/index');
+// let routesUsers = require('./routes/users');
+
+
+let app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(expressValidator());
+
+consign().include('routes').include('utils').into(app);
+
+app.listen(4000,'127.0.0.1'), ()=>{
+    console.log('rodando');
+}
