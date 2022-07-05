4d6e0823f17e15a5f89c66aa472b7d6ce6ab452b
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-05 12:52:36 -0300

    Gravando audio capturado pelo microfone com mediarecorder

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -370,9 +370,11 @@ export class WhatsAppController{
 
             this._microphoneController = new MicrophoneController();
 
-            this._microphoneController.on('play',audio=>{
+            this._microphoneController.on('ready',musica=>{
 
-                console.log('A');
+                console.log('ready event');
+
+                this._microphoneController.startRecorder();
 
             });
 
@@ -380,14 +382,14 @@ export class WhatsAppController{
 
         this.el.btnCancelMicrophone.on('click',e=>{
 
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
 
         });
 
         this.el.btnFinishMicrophone.on('click',e=>{
 
-            this._microphoneController.stop();
+            this._microphoneController.stopRecorder();
             this.closeRecordMicrophone();
 
         });
