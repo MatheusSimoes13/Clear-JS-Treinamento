af1fc90eec54b26f148151c649d7aa889be59069
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-07 09:35:53 -0300

    Configurando o firebase no projeto, firebase authentication

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -2,6 +2,7 @@ import { Format } from './../util/Format';
 import { CameraController } from './CameraController';
 import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
+import { Firebase } from '../util/Firebase';
 
 export class WhatsAppController{
 
@@ -9,12 +10,26 @@ export class WhatsAppController{
 
         console.log('whatsappcontroller ok');
 
+        this._firebase = new Firebase();
+        this.initAuth();
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
+        
 
     }
 
+    initAuth(){
+        this._firebase.initAuth().then(response=>{
+            this._user = response.user;
+
+            this.el.appContent.css({
+                display:'flex'
+            });
+        }).catch(err=>{
+            console.error(err);
+        });
+    }
 
 // Gets all the IDS from the index html
     loadElements(){
