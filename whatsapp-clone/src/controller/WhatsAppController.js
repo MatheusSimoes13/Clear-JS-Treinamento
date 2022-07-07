3af9fd492d2630f3ffd7b026f8cb908d9dcc5fd4
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-07 11:27:20 -0300

    Adicionando um contato no Whatsapp clone

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -226,6 +226,25 @@ export class WhatsAppController{
 
             let formData = new FormData(this.el.formPanelAddContact);
 
+            let contact = new User(formData.get('email'));
+
+            contact.on('datachange', data=>{
+
+                if(data.name){
+
+                    this._user.addContact(contact).then(()=>{
+
+                        this.el.btnClosePanelAddContact.click();
+                        console.info('Contato foi adicionado');
+
+                    });
+
+                } else {
+                    console.error('Usuário não encontrado');
+                }
+
+            });
+
         });
 
         this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item=>{
