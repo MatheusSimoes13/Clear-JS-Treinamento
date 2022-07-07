b3c17d3adad91ccb58de052ef69a106b9bae040e
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-07 12:55:47 -0300

    Class Chat - Criando uma conversa e usando função btoa() conversão base64

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
 import { Firebase } from '../util/Firebase';
 import { User } from '../model/User';
+import { Chat } from '../model/Chat';
 
 export class WhatsAppController{
 
@@ -334,10 +335,20 @@ export class WhatsAppController{
 
                 if(data.name){
 
-                    this._user.addContact(contact).then(()=>{
+                    Chat.createIfNotExists(this._user.email, contact.email).then(chat =>{
 
-                        this.el.btnClosePanelAddContact.click();
-                        console.info('Contato foi adicionado');
+                        contact.chatId = chat.id;
+
+                        this._user.chatId = chat.id;
+
+                        contact.addContact(this._user);
+
+                        this._user.addContact(contact).then(()=>{
+
+                            this.el.btnClosePanelAddContact.click();
+                            console.info('Contato foi adicionado');
+    
+                        });
 
                     });
 
