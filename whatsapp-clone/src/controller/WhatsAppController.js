a079d5005582e6d1ed16b963d5b36ce848774880
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-11 11:11:20 -0300

    Atualizando dados do perfil de usuário

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -8,6 +8,7 @@ import { Chat } from '../model/Chat';
 import { Message } from '../model/Message';
 import { Base64 } from '../util/base64';
 import { ContactsController } from './ContactController';
+import { Upload } from '../util/Upload';
 
 export class WhatsAppController{
 
@@ -419,6 +420,26 @@ export class WhatsAppController{
         this.el.photoContainerEditProfile.on('click', e=>{
 
             this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputProfilePhoto.on('change', e=>{
+
+            if(this.el.inputProfilePhoto.files.length > 0){
+
+                let file = this.el.inputProfilePhoto.files[0];
+
+                Upload.send(file, this._user.email).then(snapshot=>{
+
+                    this._user.photo = snapshot;
+                    this._user.save().then(() => {
+
+                        this.el.btnClosePanelEditProfile.click();
+                    });
+
+                });
+
+            }
 
         });
 
