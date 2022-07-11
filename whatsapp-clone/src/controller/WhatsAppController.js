692280827de6595e01c66ca5b52ba4dbef4a8808
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-11 08:52:56 -0300

    Enviando contato como anexo

--- whatsapp-clone/src/controller/WhatsAppController.js
+++ whatsapp-clone/src/controller/WhatsAppController.js
@@ -7,6 +7,7 @@ import { User } from '../model/User';
 import { Chat } from '../model/Chat';
 import { Message } from '../model/Message';
 import { Base64 } from '../util/base64';
+import { ContactsController } from './ContactController';
 
 export class WhatsAppController{
 
@@ -700,11 +701,21 @@ export class WhatsAppController{
            
             this.el.modalContacts.show();
 
+            this._contactsController = new ContactsController(this.el.modalContacts, this._user);
+
+            this._contactsController.on('select', contact => {
+
+                Message.sendContact(this._contactActive.chatId, this._user.email, contact)
+
+            });
+
+            this._contactsController.open();
+
         });
 
         this.el.btnCloseModalContacts.on('click',e=>{
 
-            this.el.modalContacts.hide();
+            this._contactsController.close();
 
         });
 
