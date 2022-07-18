724d32ca3d736520aab758988e2caf7fb7dc3e17
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-18 11:08:01 -0300

    Componente com propriedade

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import './index.css'
 import First from './components/basics/First'
+import WithParams from "./components/basics/WithParams"
 
 
 const el = document.getElementById("root")
 ReactDOM.render(
     <div>
         <First></First>
+        <WithParams titulo="segundo componente" aluno="Matheus" nota={9.5}/>
     </div>
 , el)
