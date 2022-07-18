af120b4deb79d95621b9135ddcfd5d41930bffa0
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-18 12:49:38 -0300

    Componente card #01 #02 #03

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,7 @@
+import './App.css'
 import React from "react"
 
+import Card from "./components/layout/Card"
 import Random from "./components/basics/Random"
 import First from './components/basics/First'
 import WithParams from "./components/basics/WithParams"
@@ -7,11 +9,25 @@ import Fragment from "./components/basics/Fragment"
 
 export default _ => (
 
-        <div id="app">
-            <Random min={1} max={60}/>
-            <Fragment/>
-            <WithParams titulo="segundo componente" aluno="Matheus" nota={9.5}/>    
-            <First></First>
+        <div className="App">
+
+            <div className="Cards">
+
+                <Card titulo="Aleatório" color="#080">
+                    <Random min={1} max={60}/>
+                </Card>
+                <Card titulo="Fragmento" color="#F0F">
+                    <Fragment />
+                </Card>
+                <Card titulo="Parâmetros" color="#3DAAF4">
+                    <WithParams titulo="aluno" aluno="Matheus" nota="10"/>
+                </Card>
+                <Card titulo="Primeiro componente">
+                    <First />
+                </Card>
+
+            </div>
+
         </div>
 
 )
