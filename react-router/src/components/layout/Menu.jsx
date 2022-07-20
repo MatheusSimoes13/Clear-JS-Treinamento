aa0543f5ada118bca605f126fbc775753312e475
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-20 12:49:32 -0300

    Aplicando estilo, react router dom, navegação simples

--- react-router/src/components/layout/Menu.jsx
+++ react-router/src/components/layout/Menu.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import './Menu.css'
+import { Link } from "react-router-dom"
 
 const Menu = props => (
     <aside className="Menu">
         <nav>
             <ul>
                 <li>
+                    <Link to="/">Início</Link>
                     <a href="/">Início</a>
                 </li>
                 <li>
