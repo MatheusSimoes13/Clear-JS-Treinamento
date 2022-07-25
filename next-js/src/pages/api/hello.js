3cae695951f977a1b34a3eba211f3621c06b625c
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-07-25 09:01:00 -0300

    Usando API #01 #02, Integração com API

---
+++ next-js/src/pages/api/hello.js
@@ -0,0 +1,11 @@
+
+
+export default function handler(req, res){
+    res.status(200).json({
+        name: 'Teste API',
+        metodo: req.method,
+        nome: req.query.nome,
+        idade: +req.query.id
+        // params: JSON.stringify(req.query)
+    })
+}
--- next-js/src/pages/index.jsx
+++ next-js/src/pages/index.jsx
@@ -10,6 +10,10 @@ export default function Inicio(){
             <Navegador texto="Exemplo" destino="/exemplo"/>
             <Navegador texto="Navegacao" destino="/navegacao" cor="#A0FA0F" />
             <Navegador texto="Navegacao Dinâmica" destino="/cliente/sp/123" cor="#AFFA0F" />
+            <Navegador texto="Componente com estado" destino="/estado" cor="#AFFAFF" />
+            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="#FFAFFA" />
+
+
         </div>
     )
 }
